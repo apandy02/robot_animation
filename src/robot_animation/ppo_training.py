a5fa30e45eff1fcd6a2ca049519d770f222b3808
aryamanpandya99 <aryaman.pandya@tufts.edu>
2025-02-26 19:54:35 -0500

    added stable-baselines3 and wrote basic ppo train script

---
+++ src/robot_animation/ppo_training.py
@@ -0,0 +1,10 @@
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+
+import gymnasium as gym
+
+env = gym.make("RobotAnimationEnv-v0")
+env = make_vec_env(lambda: env, n_envs=1)
+
+model = PPO("MlpPolicy", env, verbose=1)
+model.learn(total_timesteps=10000)
