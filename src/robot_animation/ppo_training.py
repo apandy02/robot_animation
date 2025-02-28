a066be3a156a33bc1c4f03b738093947ad6ffa87
root <aryaman.pandya@tufts.edu>
2025-02-28 03:29:48 +0000

    ran a simple ppo training loop

--- src/robot_animation/ppo_training.py
+++ src/robot_animation/ppo_training.py
@@ -1,16 +1,32 @@
+import argparse
+import sys
 import gymnasium as gym
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 
 from robot_animation.env import RobotAnimationEnv
 
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Train a PPO agent for robot animation')
+    parser.add_argument('--env', type=str, default="RobotAnimationEnv-kuka", 
+                        help='Environment ID')
+    parser.add_argument('--n_envs', type=int, default=1, 
+                        help='Number of environments to run in parallel')
+    parser.add_argument('--timesteps', type=int, default=100000, 
+                        help='Total timesteps to train for')
+    return parser.parse_args()
+
+
 def main():
-    env = gym.make("RobotAnimationEnv-kuka")
-    env = make_vec_env(lambda: env, n_envs=1)
+    args = parse_args()
+    env = gym.make(args.env)
+    env = make_vec_env(lambda: env, n_envs=args.n_envs)
 
     model = PPO("MlpPolicy", env, verbose=1)
+    model.learn(total_timesteps=args.timesteps)
+    return 1
 
-    model.learn(total_timesteps=5)
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
