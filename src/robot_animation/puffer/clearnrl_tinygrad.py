ab859951439d13e571a1423b227909fdf6305a90
Aryaman Pandya <aryaman.pandya@tufts.edu>
2025-03-24 10:47:19 -0400

    added more tinygrad components

--- src/robot_animation/puffer/clearnrl_tinygrad.py
+++ src/robot_animation/puffer/clearnrl_tinygrad.py
@@ -7,62 +7,183 @@ from typing import List, Union
 
 import numpy as np
 import torch
-import torch.nn as nn
+# import torch.nn as nn
 import torch.nn.functional as F
 from tinygrad import Tensor
 
-# This replaces gymnasium's NormalizeObservation wrapper
-# NOTE: Tried BatchNorm1d with momentum=None, but the policy did not learn. Check again later.
-class RunningNorm(nn.Module):
-    def __init__(self, shape: int, epsilon=1e-5, clip=10.0):
+class Policy(torch.nn.Module):
+    '''Wrap a non-recurrent PyTorch model for use with CleanRL'''
+    def __init__(self, policy):
         super().__init__()
-        self.register_buffer("running_mean", torch.zeros((1, shape), dtype=torch.float32))
-        self.register_buffer("running_var", torch.ones((1, shape), dtype=torch.float32))
-        self.register_buffer("count", torch.ones(1, dtype=torch.float32))
-        self.epsilon = epsilon
-        self.clip = clip
+        self.policy = policy
+        self.is_continuous = hasattr(policy, 'is_continuous') and policy.is_continuous
 
-    def forward(self, x):
-        return torch.clamp(
-            (x - self.running_mean.expand_as(x))
-            / torch.sqrt(self.running_var.expand_as(x) + self.epsilon),
-            -self.clip,
-            self.clip,
-        )
+    def get_value(self, x, state=None):
+        _, value = self.policy(x)
+        return value
 
-    @torch.jit.ignore
-    def update(self, x):
-        # NOTE: Separated update from forward to compile the policy
-        # update() must be called to update the running mean and var
-        if self.training:
-            with torch.no_grad():
-                x = x.float()
-                assert x.dim() == 2, "x must be 2D"
-                mean = x.mean(0, keepdim=True)
-                var = x.var(0, unbiased=False, keepdim=True)
-                weight = 1 / self.count
-                self.running_mean = self.running_mean * (1 - weight) + mean * weight
-                self.running_var = self.running_var * (1 - weight) + var * weight
-                self.count += 1
+    def get_action_and_value(self, x, action=None):
+         logits, value = self.policy(x)
+         action, logprob, entropy = sample_logits(logits, action, self.is_continuous)
+         return action, logprob, entropy, value
 
-    # NOTE: below are needed to torch.save() the model
-    @torch.jit.ignore
-    def __getstate__(self):
-        return {
-            "running_mean": self.running_mean,
-            "running_var": self.running_var,
-            "count": self.count,
-            "epsilon": self.epsilon,
-            "clip": self.clip,
-        }
+    def forward(self, x, action=None):
+        return self.get_action_and_value(x, action)
 
-    @torch.jit.ignore
-    def __setstate__(self, state):
-        self.running_mean = state["running_mean"]
-        self.running_var = state["running_var"]
-        self.count = state["count"]
-        self.epsilon = state["epsilon"]
-        self.clip = state["clip"]
+"""
+Next, we will one by one translate the torch code to tinygrad
+"""
+
+def layer_init(layer: nn.Linear, std=np.sqrt(2), bias_const=0.0):
+    """CleanRL's default layer initialization"""
+    layer.weight = tiny_orthogonal_(layer.weight, std)
+    layer.bias = tiny_constant_(layer.bias, bias_const)
+    return layer
+
+from tinygrad import nn 
+def tiny_orthogonal_(tensor: Tensor, gain=1, generator=None):
+    """
+    NOTE: Since initialization occurs only once, we are being lazy and using numpy linear algebra to perform certain operations.
+    """
+    if tensor.ndim < 2:
+        raise ValueError("Only tensors with 2 or more dimensions are supported")
+
+    if tensor.numel() == 0:
+        return tensor # no-op for empty tensors
+
+    rows, cols = tensor.shape[0], tensor.numel() // tensor.shape[0]
+    flattened = Tensor.randn(rows, cols) # figure out if it has the same device configs as the input tensor
+
+    if rows < cols:
+        flattened = flattened.transpose()
+
+    # for now, we use numpy to compute the qr factorization
+    q, r = np.linalg.qr(flattened.numpy())
+
+    d = np.diag(r, 0)
+    ph = np.sign(d)
+    q *= ph
+
+    if rows < cols:
+        q.transpose()
+
+    return Tensor(q).mul(gain)
+
+def tiny_constant_(tensor: Tensor, val: float):
+    """
+    """
+    return Tensor.ones(tensor.shape) * val
+    
+
+from tinygrad import nn 
+
+import math
+from tinygrad.tensor import Tensor
+
+class NormalDistribution:
+    def __init__(self, mean: Tensor, std: Tensor):
+        self.mean = mean
+        self.std = std
+
+    def sample(self):
+        noise = Tensor.randn(*self.mean.shape)
+        return self.mean + self.std * noise
+
+    def log_prob(self, value: Tensor): # TODO: Double check log_prob formulation
+        var = self.std * self.std
+        log_scale = self.std.log()
+        log_probs = - ((value - self.mean) ** 2) / (2 * var) - log_scale - 0.5 * math.log(2 * math.pi)
+        return log_probs.sum(axis=-1)
+
+    def entropy(self): # TODO: Double check entropy formulation
+        entropy = 0.5 + 0.5 * math.log(2 * math.pi) + self.std.log()
+        return entropy.sum(axis=-1)
+
+
+def logits_to_probs(logits: Tensor, is_binary: bool = False):
+    if is_binary:
+        return logits.sigmoid()
+    return logits.softmax()
+
+class TinyPolicy:
+    def __init__(self, policy):
+        self.policy = policy
+        self.is_continuous = hasattr(policy, 'is_continuous') and policy.is_continuous
+    
+    def __call__(self, x, action=None):
+        return self.get_action_and_value(x, action)
+
+    def get_value(self, x, state=None):
+        _, value = self.policy(x)
+        return value
+
+    def get_action_and_value(self, x, action=None):
+        logits, value = self.policy(x)
+        action, logprob, entropy = sample_logits(logits, action, self.is_continuous)
+        return action, logprob, entropy, value
+
+
+import tinygrad.nn
+
+class Critic:
+    def __init__(self, obs_size, hidden_size):
+        self.l1 = layer_init(tinygrad.nn.Linear(obs_size, hidden_size))
+        self.l2 = layer_init(tinygrad.nn.Linear(hidden_size, hidden_size))
+        self.l3 = layer_init(tinygrad.nn.Linear(hidden_size, 1))
+
+    def __call__(self, x: Tensor):
+        x = self.l1(x).tanh()
+        x = self.l2(x).tanh()
+        return self.l3(x)
+
+class ActorEncoder:
+    def __init__(self, obs_size, hidden_size):
+        self.l1 = layer_init(tinygrad.nn.Linear(obs_size, hidden_size))
+        self.l2 = layer_init(tinygrad.nn.Linear(hidden_size, hidden_size))
+
+    def __call__(self, x: Tensor):
+        x = self.l1(x).tanh()
+        return self.l2(x).tanh()
+
+class TinyCleanRLPolicy(TinyPolicy):
+    def __init__(self, envs, hidden_size=64):
+        super().__init__(policy=None)  # Just to get the right init
+        self.is_continuous = True
+        # self.obs_size = np.array(envs.single_observation_space.shape).prod()
+        # action_size = np.prod(envs.single_action_space.shape) # normzlize observations TODO
+        action_size = 1
+        self.obs_size = 1
+        self.critic = Critic(self.obs_size, hidden_size)
+        self.actor_encoder = ActorEncoder(self.obs_size, hidden_size)
+        self.actor_decoder_mean = layer_init(tinygrad.nn.Linear(hidden_size, action_size), std=0.01)
+        self.actor_decoder_logstd = Tensor.zeros(1, action_size)
+
+    def get_value(self, x):
+        x = x.float()
+        # x = self.obs_norm(x)
+        return self.critic(x)
+
+    def get_action_and_value(self, x: Tensor, action=None):
+        x = x.float()
+        # x = self.obs_norm(x) # commented out until observation normalization is implemented
+        batch = x.shape[0]
+        encoding = self.actor_encoder(x)
+        action_mean = self.actor_decoder_mean(encoding)
+        action_logstd = self.actor_decoder_logstd.expand_as(action_mean)
+        action_std = action_logstd.exp()
+        
+        logits = NormalDistribution(action_mean, action_std)
+        if action is None:
+            action = logits.sample()
+        
+        log_probs = logits.log_prob(action.view(batch, -1)).sum(1)
+        logits_entropy = logits.entropy().sum(1)
+
+        # NOTE: entropy can go negative, when std is small (e.g. 0.1)
+        return action, log_probs, logits_entropy, self.critic(x)
+    
+    #  def update_obs_stats(self, x):
+    #      self.obs_norm.update(x)
 
 
 def entropy(logits):
@@ -120,133 +241,56 @@ def sample_logits(logits: Union[torch.Tensor, List[torch.Tensor]],
 
     return action.T, logprob, logits_entropy
 
-class Policy(torch.nn.Module):
-    '''Wrap a non-recurrent PyTorch model for use with CleanRL'''
-    def __init__(self, policy):
+
+# This replaces gymnasium's NormalizeObservation wrapper
+# NOTE: Tried BatchNorm1d with momentum=None, but the policy did not learn. Check again later.
+class RunningNorm(nn.Module):
+    def __init__(self, shape: int, epsilon=1e-5, clip=10.0):
         super().__init__()
-        self.policy = policy
-        self.is_continuous = hasattr(policy, 'is_continuous') and policy.is_continuous
+        self.register_buffer("running_mean", torch.zeros((1, shape), dtype=torch.float32))
+        self.register_buffer("running_var", torch.ones((1, shape), dtype=torch.float32))
+        self.register_buffer("count", torch.ones(1, dtype=torch.float32))
+        self.epsilon = epsilon
+        self.clip = clip
 
-    def get_value(self, x, state=None):
-        _, value = self.policy(x)
-        return value
-
-    def get_action_and_value(self, x, action=None):
-         logits, value = self.policy(x)
-         action, logprob, entropy = sample_logits(logits, action, self.is_continuous)
-         return action, logprob, entropy, value
-
-    def forward(self, x, action=None):
-        return self.get_action_and_value(x, action)
-    
-class CleanRLPolicy(Policy):
-    def __init__(self, envs, hidden_size=64):
-        super().__init__(policy=None)  # Just to get the right init
-        self.is_continuous = True
-
-        self.obs_size = np.array(envs.single_observation_space.shape).prod()
-        action_size = np.prod(envs.single_action_space.shape)
-
-        self.obs_norm = torch.jit.script(RunningNorm(self.obs_size))
-
-        self.critic = nn.Sequential(
-            layer_init(nn.Linear(self.obs_size, hidden_size)),
-            nn.Tanh(),
-            layer_init(nn.Linear(hidden_size, hidden_size)),
-            nn.Tanh(),
-            layer_init(nn.Linear(hidden_size, 1), std=1.0),
+    def forward(self, x):
+        return torch.clamp(
+            (x - self.running_mean.expand_as(x))
+            / torch.sqrt(self.running_var.expand_as(x) + self.epsilon),
+            -self.clip,
+            self.clip,
         )
 
-        self.actor_encoder = nn.Sequential(
-            layer_init(nn.Linear(self.obs_size, hidden_size)),
-            nn.Tanh(),
-            layer_init(nn.Linear(hidden_size, hidden_size)),
-            nn.Tanh(),
-        )
-        self.actor_decoder_mean = layer_init(nn.Linear(hidden_size, action_size), std=0.01)
-        self.actor_decoder_logstd = nn.Parameter(torch.zeros(1, action_size))
+    @torch.jit.ignore
+    def update(self, x):
+        # NOTE: Separated update from forward to compile the policy
+        # update() must be called to update the running mean and var
+        if self.training:
+            with torch.no_grad():
+                x = x.float()
+                assert x.dim() == 2, "x must be 2D"
+                mean = x.mean(0, keepdim=True)
+                var = x.var(0, unbiased=False, keepdim=True)
+                weight = 1 / self.count
+                self.running_mean = self.running_mean * (1 - weight) + mean * weight
+                self.running_var = self.running_var * (1 - weight) + var * weight
+                self.count += 1
 
-    def get_value(self, x):
-        x = x.float()
-        x = self.obs_norm(x)
-        return self.critic(x)
+    # NOTE: below are needed to torch.save() the model
+    @torch.jit.ignore
+    def __getstate__(self):
+        return {
+            "running_mean": self.running_mean,
+            "running_var": self.running_var,
+            "count": self.count,
+            "epsilon": self.epsilon,
+            "clip": self.clip,
+        }
 
-    def get_action_and_value(self, x, action=None):
-        x = x.float()
-        x = self.obs_norm(x)
-        batch = x.shape[0]
-
-        encoding = self.actor_encoder(x)
-        action_mean = self.actor_decoder_mean(encoding)
-        action_logstd = self.actor_decoder_logstd.expand_as(action_mean)
-        action_std = torch.exp(action_logstd)
-
-        # NOTE: This produces nans, so disabling for now
-        # Work around for CUDA graph capture
-        # if torch.cuda.is_current_stream_capturing():
-        #     if action is None:
-        #         action = action_mean + action_std * torch.randn_like(action_mean)
-
-        #     # Avoid using the torch.distributions.Normal
-        #     log_probs = (-0.5 * (((action - action_mean) / action_std) ** 2 + 2 * action_std.log() + torch.log(torch.tensor(2 * torch.pi)))).sum(1)
-        #     logits_entropy = (action_std.log() + 0.5 * torch.log(torch.tensor(2 * torch.pi * torch.e))).sum(1)
-
-        # else:
-
-        logits = torch.distributions.Normal(action_mean, action_std)
-        if action is None:
-            action = logits.sample()
-        log_probs = logits.log_prob(action.view(batch, -1)).sum(1)
-        logits_entropy = logits.entropy().sum(1)
-
-        # NOTE: entropy can go negative, when std is small (e.g. 0.1)
-        return action, log_probs, logits_entropy, self.critic(x)
-
-    def update_obs_stats(self, x):
-        self.obs_norm.update(x)
-
-"""
-Next, we will one by one translate the torch code to tinygrad
-"""
-
-
-def layer_init(layer: nn.Linear, std=np.sqrt(2), bias_const=0.0):
-    """CleanRL's default layer initialization"""
-    layer.weight = tiny_orthogonal_(layer.weight, std)
-    layer.bias = tiny_constant_(layer.bias, bias_const)
-    return layer
-
-from tinygrad import nn 
-def tiny_orthogonal_(tensor: Tensor, gain=1, generator=None):
-    """
-    NOTE: Since initialization occurs only once, we are being lazy and using numpy linear algebra to perform certain operations.
-    """
-    if tensor.ndim < 2:
-        raise ValueError("Only tensors with 2 or more dimensions are supported")
-
-    if tensor.numel() == 0:
-        return tensor # no-op for empty tensors
-
-    rows, cols = tensor.shape[0], tensor.numel() // tensor.shape[0]
-    flattened = Tensor.randn(rows, cols) # figure out if it has the same device configs as the input tensor
-
-    if rows < cols:
-        flattened = flattened.transpose()
-
-    # for now, we use numpy to compute the qr factorization
-    q, r = np.linalg.qr(flattened.numpy())
-
-    d = np.diag(r, 0)
-    ph = np.sign(d)
-    q *= ph
-
-    if rows < cols:
-        q.transpose()
-
-    return Tensor(q).mul(gain)
-
-def tiny_constant_(tensor: Tensor, val: float):
-    """
-    """
-    return Tensor.ones(tensor.shape) * val
-
+    @torch.jit.ignore
+    def __setstate__(self, state):
+        self.running_mean = state["running_mean"]
+        self.running_var = state["running_var"]
+        self.count = state["count"]
+        self.epsilon = state["epsilon"]
+        self.clip = state["clip"]
